70acfdfd3bcdb9884ca2cd7e24c438d215d2d921
thomas-binga <Salome@29>
2021-03-26 15:06:00 +0100

    Initial commit

---
+++ src/modele/plateau/Echelle.java
@@ -0,0 +1,12 @@
+package modele.plateau;
+
+public class Echelle extends EntiteStatique{
+    public Echelle(Jeu _jeu) {
+        super(_jeu);
+
+    }
+    @Override
+    public boolean peutPermettreDeMonterDescendre() {
+        return true;
+    }
+}
